e2c4671559c1b77a1bd279edfba6e77590d68f1f
Arsen4ik <46644855+Arsen4ik@users.noreply.github.com>
2019-09-16 20:48:07 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Arazal/learnWordsFromGoogle/precache-manifest.e780ca745d3975fc69b747efc4937082.js"
+  "/learnWordsFromGoogle/precache-manifest.4e3d1fa67550f288d592d984365c4edc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Arazal/learnWordsFromGoogle/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/learnWordsFromGoogle/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
